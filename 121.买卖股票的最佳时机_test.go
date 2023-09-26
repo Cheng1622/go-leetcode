3545e44664363bb303880c20abb3020778e299f6
Cheng1622 <1622762650@qq.com>
2023-09-26 23:38:07 +0800

    8.26

--- 121.买卖股票的最佳时机_test.go
+++ 121.买卖股票的最佳时机_test.go
@@ -48,15 +48,59 @@
  *
  *
  */
+package sf
+
+import "math"
 
 // @lc code=start
-func maxProfit(prices []int) int {
-	length := len(prices)
-	if length == 0 {
-		return 0
-	}
 
+//动态规划
+// func maxProfit(prices []int) int {
+// 	length := len(prices)
+// 	if length == 0 {
+// 		return 0
+// 	}
+// 	dp := make([][]int, length)
+// 	for i := 0; i < length; i++ {
+// 		dp[i] = make([]int, 2)
+// 	}
+// 	dp[0][0] = -prices[0]
+// 	dp[0][1] = 0
+// 	for i := 1; i < length; i++ {
+// 		dp[i][0] = max(dp[i-1][0], -prices[i])
+// 		dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i])
+// 	}
+// 	return dp[length-1][1]
+// }
+// func max(a, b int) int {
+// 	if a > b {
+// 		return a
+// 	}
+// 	return b
+// }
+
+// 贪心
+func maxProfit(prices []int) int {
+	mins := math.MaxInt64
+	maxs := math.MinInt64
+	length := len(prices)
+	for i := 0; i < length; i++ {
+		mins = min(mins, prices[i])
+		maxs = max(maxs, prices[i]-mins)
+	}
+	return maxs
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 // @lc code=end
-
